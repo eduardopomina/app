790d5f5c7e6bf538b6166fa37ff5685f5f7d695e
Javan <javan_dhein@outlook.com>
2024-03-25 19:48:06 -0300

    Update header visibility in App.js

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions={{ headerShown: false }}
+        screenOptions={{ headerShown: true }}
         initialRouteName="Home"
       >
         <Stack.Screen name="Home" component={Home} />
