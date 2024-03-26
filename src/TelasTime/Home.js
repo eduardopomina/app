a6c3dde4768390edd61e78325d98d788be6d403d
Javan <javan_dhein@outlook.com>
2024-03-25 21:19:24 -0300

    add sttyle

--- src/TelasTime/Home.js
+++ src/TelasTime/Home.js
@@ -1,16 +1,35 @@
 import React from "react";
-import { Text, Image } from "react-native";
+import { Text, Image, View, StyleSheet } from "react-native";
 
 function Home(props, { route, navigation }) {
-  const { time, facanha, simbolo } = props;
-  return (
-    <>
-      <Text>Tela do Time</Text>
-      <Text> {time} </Text>
-      <Image style={{ width: 200, height: 200 }} source={{ uri: simbolo }} />
-      <Text>{facanha}</Text>
-    </>
-  );
+ const { time, facanha, simbolo } = props;
+ return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Tela do Time</Text>
+      <Text style={styles.text}>{time}</Text>
+      <Image style={styles.image} source={{ uri: simbolo }} />
+      <Text style={styles.text}>{facanha}</Text>
+    </View>
+ );
 }
+
+const styles = StyleSheet.create({
+ container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+ },
+ text: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+ },
+ image: {
+    width: 200,
+    height: 200,
+    margin: 10,
+ },
+});
 
 export default Home;
